9567889e12ee96e037ccb0462facfe87f8847718
Oscar Resendiz <resendizor17@gmail.com>
2023-02-24 16:00:42 -0600

    adding playMidi new version

--- playMidi.py
+++ playMidi.py
@@ -1,9 +1,9 @@
 import mido
 
 # Get the name of the input port for your digital piano
-port = mido.open_output('Midi Through:Midi Through Port-0 14:0')
+#port = mido.open_output('Midi Through:Midi Through Port-0 14:0')
 # or
-#port = mido.open_output("CH345:CH345 MIDI 1 20:0")
-mid = mido.MidiFile('Dr Dre.mid')
+port = mido.open_output("CH345:CH345 MIDI 1 20:0")
+mid = mido.MidiFile('Ramin_Djawadi_-_Westworld_Theme.mid')
 for msg in mid.play():
     port.send(msg)
